449202d4a4c07efa42d6425c0bb348b897495d05
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2024-05-15 19:25:38 +0500

    multi-dimensional array added

---
+++ Arrays/MultiDimensionalArray/MultiDimensionalArray.ts
@@ -0,0 +1,17 @@
+// We can also add array into array this is called nested array or multidimensional array
+const matrix: number[][] = [
+  [121, 25, 55],
+  [444, 551, 55],
+  [552, 55, 11],
+];
+
+console.log(matrix[0][2], "matrix");
+
+const DeepMatrix: number[][][] = [
+  [[55, 1154, 22681, 27]],
+  [[25, 21, 98, 5]],
+  [[559, 11, 485, 55]],
+];
+
+console.log(DeepMatrix[0][0][1], "matrix");
+
